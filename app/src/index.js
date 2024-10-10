e6a61ee30344476dfdac79b32d3336800a57f6e5
Anuraj Venkatpurwar <88710429+Anuraj4@users.noreply.github.com>
2024-10-10 17:45:44 +0530

    Update index.js

--- app/src/index.js
+++ app/src/index.js
@@ -6,11 +6,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'; // Add this line to import Bootst
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 reportWebVitals();
